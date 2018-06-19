625a4b24fba1772eff7fc5a711466689ef640944
Jonathan Ribarro <Jcribarro@gmail.com>
2018-06-19 11:12:21 -0400

    beginning the animation for the players getting into the game, going to instantiate a second image on the opposite side of the screen, need to make it happen smoothly over about three seconds, which I am working on right now

--- ticTacToeU/ticTacToe/Assets/Scripts/entryAnimation.cs
+++ ticTacToeU/ticTacToe/Assets/Scripts/entryAnimation.cs
@@ -1,24 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.UI;
-public class entryAnimation : MonoBehaviour {
+public class entryAnimation : MonoBehaviour
+{
 
 
-    Image player1Image;
-    Image player2Image;
+    [SerializeField] private GameObject playerImage;
 
-	// Use this for initialization
-	void Start () {
-        player1Image.material.mainTexture = ticTacManager.player1.playerCharacterTexture;
-        player2Image.material.mainTexture = ticTacManager.player2.playerCharacterTexture;
+    Vector3 player2Pos = new Vector3(250.0f, 0.0f, 0.0f);
 
-       
+    Color totallyClear = new Color(255.0f, 255.0f, 255.0f, 0.0f);
+    Color totallyFull = new Color(255.0f, 255.0f, 255.0f, 255.0f);
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        GameObject player2Image = Instantiate(playerImage, player2Pos, Quaternion.identity);
+
+        playerImage.GetComponent<Material>().mainTexture = ticTacManager.player1.playerCharacterTexture;
+        player2Image.GetComponent<Material>().mainTexture = ticTacManager.player2.playerCharacterTexture;
+
+        playerImage.GetComponent<Material>().color = totallyClear;
+        player2Image.GetComponent<Material>().color = totallyClear;
+
+        
+
+    }
+
+    void lerpAlphaToFull()
+    {
+        Color lerpedColor = totallyClear;
+        lerpedColor.a = Mathf.Lerp(lerpedColor.a, 1.0f, 3.0f * Time.deltaTime);
+        playerImage.GetComponent<Material>().color = lerpedColor;
+
+    }
+
+    void lerpAlphaToClear()
+    {
+        Color lerpedColor = totallyFull;
+        lerpedColor.a = Mathf.Lerp(lerpedColor.a, 0.0f, 3.0f * Time.deltaTime);
+        playerImage.GetComponent<Material>().color = lerpedColor;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lerpAlphaToFull();
+    }
 }
