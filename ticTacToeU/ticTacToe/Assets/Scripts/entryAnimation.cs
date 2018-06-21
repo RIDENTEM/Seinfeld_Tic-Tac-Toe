9bc842e4a0e9c2d9899eb3a7b5d36d018fb0d29d
Jonathan Ribarro <Jcribarro@gmail.com>
2018-06-21 11:56:34 -0400

    Still figuring out Animator stuff, also changed the script to handle Animator changes and such once I figure out how to use it.

--- ticTacToeU/ticTacToe/Assets/Scripts/entryAnimation.cs
+++ ticTacToeU/ticTacToe/Assets/Scripts/entryAnimation.cs
@@ -9,59 +9,20 @@ public class entryAnimation : MonoBehaviour
 
     [SerializeField] private RawImage playerImage;
     [SerializeField] private RawImage player2Image;
-    bool fadeInDone = false;
-    [SerializeField] private float duration = 3.0f;
-    private float elapsedTime = 0.0f;
-    Vector3 player2Pos = new Vector3(250.0f, 0.0f, 0.0f);
-
-    Color totallyClear = new Color(255.0f, 255.0f, 255.0f, 0.0f);
-    Color totallyFull = new Color(255.0f, 255.0f, 255.0f, 255.0f);
+    [SerializeField] private Animator player1Animator;
+    [SerializeField] private Animator player2Animator;
+    
 
     void Start()
     {
-
-        playerImage.texture = ticTacManager.player1.playerCharacterTexture;
-        player2Image.texture = ticTacManager.player2.playerCharacterTexture;
-
-        playerImage.color = totallyClear;
-        player2Image.color = totallyClear;
-
-
-
+         
+     //   player1Animator.
+       
     }
-
-    void lerpAlphaToFull()
-    {
-
-        if (elapsedTime < 1)
-        {
-            Color lerpedColor = new Color(1.0f, 1.0f, 1.0f, 0.0f);
-            elapsedTime += Time.deltaTime / duration;
-            lerpedColor.a = Mathf.Lerp(lerpedColor.a, 1.0f, elapsedTime);
-            playerImage.color = lerpedColor;
-            player2Image.color = lerpedColor;
-        }
-        if (elapsedTime >= 1)
-            fadeInDone = true;
-
-    }
-
-    void lerpAlphaToClear()
-    {
-
-        Color lerpedColor = totallyFull;
-        lerpedColor.a = Mathf.Lerp(lerpedColor.a, 0.0f, 3.0f * Time.deltaTime);
-        playerImage.color = lerpedColor;
-
-
-
-    }
-
+     
     // Update is called once per frame
     void Update()
-    {
-        lerpAlphaToFull();
-        if (fadeInDone == true)
-            lerpAlphaToClear();
+    { 
+
     }
 }
